40c1af05f6164f73f2ff11ca136c302de9f27c4e
Mimoja <max-schander@freenet.de>
2017-04-28 23:22:01 +0200

    Add commit searching script using svn grep from llvm repo

---
+++ 0004-find_bug_commits.sh
@@ -0,0 +1,1 @@
+cat critical_bug_ids | sed 's/\?/\\?/g' | parallel --eta -j20 'f=$(echo {} | sed "s/https:\/\/bugs.webkit.org\/show_bug.cgi\\\?id\=//g"); cat svn_log | python  grep-svn-log.py {} > bugs/$f'
