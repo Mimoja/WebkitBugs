53f94ff06fb4c5c0f71905e054b4ac7275f8f39c
Mimoja <max-schander@freenet.de>
2017-04-28 23:46:14 +0200

    Add svn log

--- 0001-get_svn_history.sh
+++ 0001-get_svn_history.sh
@@ -1,1 +1,1 @@
-svn log -v https://svn.webkit.org/repository/webkit/trunk > svn_log
+svn log -v https://svn.webkit.org/repository/webkit/trunk |gzip > svn_log.gz
--- 0002-extract_bug_ids.sh
+++ 0002-extract_bug_ids.sh
@@ -1,1 +1,1 @@
-cat svn_log | egrep -o 'https://bugs.webkit.org/show_bug.cgi?id=[1-9]+' | sort -u > bug_ids
+zcat svn_log.gz | egrep -o 'https://bugs.webkit.org/show_bug.cgi?id=[1-9]+' | sort -u > bug_ids
