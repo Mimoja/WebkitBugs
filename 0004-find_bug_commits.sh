676441154adba000ae100e38bbf7a1ae75452349
Mimoja <max-schander@freenet.de>
2017-04-29 00:23:16 +0200

    fix bug extractor

--- 0004-find_bug_commits.sh
+++ 0004-find_bug_commits.sh
@@ -1,1 +1,1 @@
-cat ../cve_bugs | sed 's/\?/\\?/g' | parallel --eta -j20 'f=$(echo {} | sed "s/https:\/\/bugs.webkit.org\/show_bug.cgi\\\?id\=//g"); cat svn_log | $(python  grep-svn-log.py {} > bugs/$f)'
+cat critical_bug_ids | sed 's/\?/\\?/g' | parallel --eta -j20 'f=$(echo {} | sed "s/https:\/\/bugs.webkit.org\/show_bug.cgi\\\?id\=//g"); zcat svn_log.gz | $(python  grep-svn-log.py {} > bugs/$f)'
